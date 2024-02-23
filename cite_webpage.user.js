fa63e8e0e8aef6c2f563300c92b1d353a89cae67
Mutsuhiro Iwamoto <mutsuhiro6@icloud.com>
2024-02-24 00:34:32 +0900

    delete redundant function

--- cite_webpage.user.js
+++ cite_webpage.user.js
@@ -9,36 +9,14 @@
 // @match        *://*/*
 // @grant        GM_setClipboard
 // @grant        GM_notification
-// @grant        GM_setValue
-// @grant        GM_getValue
 // ==/UserScript==
-
-const MAX_HISTORY_LENGTH = 20
-const HISTORY_KEY = 'citation_history'
 
 function copyUrlAndTitle() {
     const url = location.href
     const title = document.title
     const text = title + ' - ' + url
-    updateHistory(text)
     GM_setClipboard(text)
     GM_notification({ title: 'Copied!', text: text, timeout: 1500 })
-}
-
-async function updateHistory(content) {
-    const timestamp = new Date().toJSON()
-    const new_content = {
-        timestamp: timestamp,
-        content: content
-    }
-    const histories = await GM_getValue(HISTORY_KEY, undefined)
-    if (!histories || histories.length < 1) {
-        GM_setValue(HISTORY_KEY, [new_content])
-    } else {
-        histories.push(new_content)
-        if (histories.length > MAX_HISTORY_LENGTH) histories.shift()
-        GM_setValue(HISTORY_KEY, histories)
-    }
 }
 
 (() => {
