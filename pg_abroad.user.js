5f6671ebeb0a84b1f2e7d747f01bb07b0c6efd08
Mutsuhiro Iwamoto <mutsuhiro6@icloud.com>
2025-03-30 14:07:11 +0900

    Update to dynamically generate link

--- pg_abroad.user.js
+++ pg_abroad.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pg_abroad
 // @namespace    https://github.com/mutsuhiro6/tm-scripts
-// @version      1.0
+// @version      1.1
 // @description  Add link to move PostgreSQL documents between Global and JP.
 // @author       mutsuhiro6
 // @icon         https://icons.duckduckgo.com/ip2/www.postgresql.org.ico
@@ -13,12 +13,11 @@
 // @grant        GM_openInTab
 // ==/UserScript==
 
-function gena(href, text) {
-  return '<a href="' + href + '" target="_blank" rel="noopener noreferrer">' + text + '</a>'
+function gena(text) {
+  return '<span id="pg_abroad" style="cursor:pointer;">' + text + '</span>'
 }
 
-(async () => {
-  'use strict'
+function goAbroad() {
   // URL is assumed like "https://www.postgresql.org/docs/14/history.html" or "https://www.postgresql.jp/document/14/html/history.html"
   // so path will be ['', 'docs', '14', 'history.html'] or ['', 'document', '14', 'html', 'history.html'].
   const path = document.location.pathname.split('/')
@@ -28,13 +27,23 @@ function gena(href, text) {
   const file = path.slice(-1)[0] // history.html, index.html...
   if (location.host == 'www.postgresql.org') {
     const url = 'https://www.postgresql.jp/document/' + version + '/html/' + file + location.hash
-    GM_registerMenuCommand('🇯🇵', () => { GM_openInTab(url, { active: true, insert: true, setParent: true }) }, { accessKey: 'a' })
-    const elm = '<div class="row"><div class="col-12">Japanese Version: ' + gena(url, '🇯🇵') + '</div></div>'
-    document.querySelector('#pgContentWrap > div.row > div.col-md-6.mb-2').insertAdjacentHTML('beforeend', elm)
+    GM_openInTab(url, { active: true, insert: true, setParent: true })
   } else if (location.host == 'www.postgresql.jp') {
     const url = 'https://www.postgresql.org/docs/' + version + '/' + file + location.hash
-    GM_registerMenuCommand('🐘', () => { GM_openInTab(url, { active: true, insert: true, setParent: true }) }, { accessKey: 'a' })
-    const elm = '<span class="label">' + gena(url, '🐘') + '|' + '</span>'
+    GM_openInTab(url, { active: true, insert: true, setParent: true })
+  }
+}
+
+(async () => {
+  'use strict'
+  if (location.host == 'www.postgresql.org') {
+    GM_registerMenuCommand('🇯🇵', () => { goAbroad() }, { accessKey: 'a' })
+    const elm = '<div class="row"><div class="col-12">Japanese Version: ' + gena('🇯🇵') + '</div></div>'
+    document.querySelector('#pgContentWrap > div.row > div.col-md-6.mb-2').insertAdjacentHTML('beforeend', elm)
+  } else if (location.host == 'www.postgresql.jp') {
+    GM_registerMenuCommand('🐘', () => { goAbroad() }, { accessKey: 'a' })
+    const elm = '<span class="label">' + gena('🐘') + '</span> |'
     document.querySelector('#docContent > div.versions').insertAdjacentHTML('afterbegin', elm)
   }
+  document.querySelector('#pg_abroad').addEventListener('click', e => { goAbroad() })
 })()
